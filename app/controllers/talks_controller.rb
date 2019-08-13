c1fb1d0a308b0dc3fa00200a267027c0714959dc
moemi ohtsu <s0623192@gmail.com>
2019-08-13 20:21:08 +0900

    Add: talkのnewアクション、indexページ作成

---
+++ app/controllers/talks_controller.rb
@@ -0,0 +1,20 @@
+class TalksController < ApplicationController
+  def index
+    @talks = Talk.all
+  end
+
+  def new
+    @talk = Talk.new
+  end
+
+  def create
+    Talk.create(talk_params)
+    redirect_to new_talk_path
+  end
+
+  private
+
+  def talk_params
+    params.require(:talk).permit(:title, :content, :complete)
+  end
+end
