7f6908aa5bd9d4ca279e1706378b8423173df083
moemi ohtsu <s0623192@gmail.com>
2019-08-13 15:37:26 +0900

    Add: タイムゾーンを日本語化、i18nを記述

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,10 @@ module GraduateApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+  # デフォルトの言語を日本語に設定する
+  config.i18n.default_locale = :ja
+  # タイムゾーン設定
+  config.time_zone = 'Tokyo'
+  # Rails(Activerecord)がDBへのRead・Writeを行う際タイムゾーン
+  config.active_record.default_timezone = :local
 end
