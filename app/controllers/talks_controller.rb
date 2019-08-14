091697cedb8510eb0e7681a14b4ec0325c47eda5
moemi ohtsu <s0623192@gmail.com>
2019-08-14 11:33:05 +0900

    Add: talk編集機能実装

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,4 +1,6 @@
 class TalksController < ApplicationController
+  before_action :set_talk, only: [:show, :edit, :update, :destroy]
+
   def index
     @talks = Talk.all
   end
@@ -8,8 +10,26 @@ class TalksController < ApplicationController
   end
 
   def create
-    Talk.create(talk_params)
-    redirect_to new_talk_path
+    @talk = Talk.new(talk_params)
+    if @talk.save
+      redirect_to talks_path, notice: "相談を投稿しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @talk.update(talk_params)
+      redirect_to talks_path, notice: "編集しました。"
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -17,4 +37,9 @@ class TalksController < ApplicationController
   def talk_params
     params.require(:talk).permit(:title, :content, :complete)
   end
+
+  def set_talk
+    @talk = Talk.find(params[:id])
+  end
+
 end
