a528529c59d1e303c38739366a7b23fbc5751070
moemi ohtsu <s0623192@gmail.com>
2019-08-15 13:22:08 +0900

    talk-comment間で1対多アソシエーション

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,7 @@
 class Talk < ApplicationRecord
   has_many :talk_categories, dependent: :destroy
   has_many :categories, through: :talk_categories
+  has_many :comments
   validates :title, presence: true, uniqueness: true, length: { in: 1..255 }
   validates :content, presence: true, uniqueness: true
 end
