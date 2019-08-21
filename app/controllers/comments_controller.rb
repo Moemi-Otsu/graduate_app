7820c041faf337eb5615d2917b6e9e5085734d38
moemi ohtsu <s0623192@gmail.com>
2019-08-21 11:33:41 +0900

    Add: commentのnewアクション-ネストされたroutingでnewページを表示（途中コミット)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,30 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:edit, :update, :destroy]
 
+  def new
+    @talk = Talk.find(params[:talk_id])
+    @comment = @talk.comments.build
+  end
+
   # コメントを保存、投稿するためのアクションです。
   def create
     # Talkをパラメータの値から探し出し,Talkに紐づくcommentsとしてbuildします。
     @talk = Talk.find(params[:talk_id])
     @comment = @talk.comments.build(comment_params)
     # クライアント要求に応じてフォーマットを変更
-    respond_to do |format|
-      if @comment.save
-        format.js { render :index }
-      else
-        format.html { redirect_to talk_path(@talk), notice: "投稿できませんでした…" }
-      end
-    end
+    # respond_to do |format|
+    #   if @comment.save
+    #     format.js { render :index }
+    #   else
+    #     format.html { redirect_to talk_path(@talk), notice: "投稿できませんでした…" }
+    #   end
+    # end
   end
 
   def edit
   end
 
   def destroy
-    if @comment.destroy
-      render :index
-    end
   end
 
   private
