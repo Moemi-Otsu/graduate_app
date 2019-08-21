b54986a0ed6d4f5ebff99cb36e3d84524b6e170c
moemi ohtsu <s0623192@gmail.com>
2019-08-21 17:45:47 +0900

    Add: routingを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
+  # devise
   devise_for :users, controllers: {
     registrations: 'users/registrations'
    }
+  # root
   root to: "talks#index"
+  # talk / comments
   resources :talks do
     collection do
       post :confirm
     end
     resources :comments
   end
-  resources :profiles
+  # profile
+  resources :profiles do
+    member do
+      get :talks
+      get :comments
+    end
+  end
+  # category
   resources :categories
 end
