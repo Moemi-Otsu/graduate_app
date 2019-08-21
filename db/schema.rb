5df0151320bca1902a5a50dff68b6fec0b85053b
moemi ohtsu <s0623192@gmail.com>
2019-08-21 12:29:37 +0900

    Add: commentsテーブルにFKのuser_idを追加してmigration実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_033957) do
+ActiveRecord::Schema.define(version: 2019_08_21_032707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 2019_08_20_033957) do
     t.bigint "talk_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["talk_id"], name: "index_comments_on_talk_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -79,6 +81,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_033957) do
   end
 
   add_foreign_key "comments", "talks"
+  add_foreign_key "comments", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "talks", "users"
 end
