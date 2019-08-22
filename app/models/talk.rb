84076e288c271b6355e3ce2992cebcdc586cc65f
moemi ohtsu <s0623192@gmail.com>
2019-08-22 12:15:22 +0900

    Add: talkのmodelにアソシエーションメソッド記述

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -4,6 +4,8 @@ class Talk < ApplicationRecord
   has_many :categories, through: :talk_categories
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  # アソシエーションメソッド
+  has_many :like_users, through: :likes, source: :user
   validates :title, presence: true, uniqueness: true, length: { in: 1..255 }
   validates :content, presence: true, uniqueness: true
 end
