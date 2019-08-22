888327c52bbaf6d6914d926fbc74998c4268d418
moemi ohtsu <s0623192@gmail.com>
2019-08-22 12:13:30 +0900

    Add: talkとuserにhas_manu設定してアソシエーション

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,6 +3,7 @@ class Talk < ApplicationRecord
   has_many :talk_categories, dependent: :destroy
   has_many :categories, through: :talk_categories
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   validates :title, presence: true, uniqueness: true, length: { in: 1..255 }
   validates :content, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :talks
   has_one :profile, dependent: :destroy
   has_many :comments
+  has_many :likes, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
