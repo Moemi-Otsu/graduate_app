32111faef670b80d2bdf33052b8a76fb59dbff90
moemi ohtsu <s0623192@gmail.com>
2019-08-23 13:02:41 +0900

    Add: default値のみ設定できsた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_025058) do
+ActiveRecord::Schema.define(version: 2019_08_23_035658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2019_08_23_025058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "likes_count"
+    t.integer "likes_count", default: 0
     t.index ["user_id"], name: "index_talks_on_user_id"
   end
 
