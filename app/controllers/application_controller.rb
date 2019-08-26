165cf9c178ac8f5930311cde2557dd0f229fbfae
moemi ohtsu <s0623192@gmail.com>
2019-08-26 21:39:53 +0900

    Add: userテーブルにroleカラムを追加したため、application_controller.rbに追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,16 @@ class ApplicationController < ActionController::Base
 
   protected
 
-def current_ability
-  @current_ability ||= Ability.new(current_user)
-end
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
 
   def configure_permitted_parameters
     added_attrs = [ :name, :email, :password, :password_confirmation　]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    # cancancan用記述
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
   end
 end
