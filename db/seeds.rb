3eb0637a5dde63c00d1407c5f2d4ac143e6b0784
moemi ohtsu <s0623192@gmail.com>
2019-08-28 12:06:24 +0900

    Add: 微調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,39 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# User
-20.times do |n|
-  email = Faker::Internet.email
-  name = Faker::Name.name
-  password = "password"
-  role = "member"
-  User.create!(name: name,
-               email: email,
-               role: role,
-               password: password,
-               password_confirmation: password,
-               )
-end
-
-# Profile
-user_id_array = User.pluck(:id).sort_by{rand}
-age = [1..100]
-20.times do |n|
-  age = 
-  image = 
-  working_company = 
-  previous_company = 
-  length_worked = 
-  address = 
-  area = 
-  occupation = 
-  industry = 
-  user_id = user_id_array[n]
-  Profile.create!(name: name,
-               email: email,
-               role: role,
-               password: password,
-               
-               )
-end
