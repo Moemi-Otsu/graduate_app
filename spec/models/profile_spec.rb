e84e9a087d3d849418925b2983cd33eba77a2f33
moemi ohtsu <s0623192@gmail.com>
2019-08-29 23:09:23 +0900

    Add: profileのmodelスペック記述

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -7,17 +7,25 @@ RSpec.describe User, type: :model do
     it "age（年齢）が存在しなければ、エラーを出す" do
       profile = Profile.new
       expect(profile.valid?).to be false
-      expect(user.errors[:age]).to include("を入力してください")
+      expect(profile.errors[:age]).to include("を入力してください")
     end
 
     it "area（お住いのエリア）が存在しなければ、エラーを出す" do
+      profile = Profile.new(area: nil)
+      expect(profile.valid?).to be false
+      expect(profile.errors[:area]).to include("を入力してください")
     end
 
     it "industry（お住いのエリア）が存在しなければ、エラーを出す" do
+      profile = Profile.new
+      expect(profile.valid?).to be false
+      expect(profile.errors[:industry]).to include("を入力してください")
     end
 
     it "occupation（業種）が存在しなければ、エラーを出す" do
+      profile = Profile.new
+      expect(profile.valid?).to be false
+      expect(profile.errors[:occupation]).to include("を入力してください")
     end
   end
-
 end
