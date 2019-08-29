5d8ff0d91c67f99179f6ec779ad9ddb1e96e56e5
moemi ohtsu <s0623192@gmail.com>
2019-08-29 15:54:38 +0900

    Add: userのmodelテスト2

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,19 +2,22 @@ FactoryBot.define do
 
   factory :user do
     email { 'zzz@mail.com' }
-    encrypted_password { 'zzzzzz' }
+    password { 'zzzzzz' }
+    password_confirmation { 'zzzzzz' }
     name { 'zzz' }
     role { 'admin' }
   end
 
   factory :second_user, class: User do
     email { Faker::Internet.email }
-    encrypted_password { 'password' }
+    password { 'password' }
+    password_confirmation { 'password' }
     name { Faker::Name.name }
     role { 'member' }
   end
 
   factory :third_user, class: User do
+    # ここに処理が入ります。
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,22 @@ RSpec.describe User, type: :model do
   let(:second_user) { create(:second_user) }
 
   describe 'バリデーション確認' do
-    it "nameが存在しなければ、エラー" do
+
+    it "nameが存在しなければ、エラーを出す" do
       user = User.new
+      # byebug
       expect(user.valid?).to be false
       expect(user.errors[:name]).to include("を入力してください")
       expect(user.errors.full_messages).to include("名前を入力してください")
     end
+
+    it "nameに重複があれば、エラーを出す" do
+      duplicated_user = User.new(name: user.name)
+      # byebug
+      expect(duplicated_user.valid?).to be false
+      expect(duplicated_user.errors[:name]).to include('はすでに存在します')
+    end
+
   end
 
 end
